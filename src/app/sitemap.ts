30f846428b0378c0ba6ee39baa2356c6704935fe
Gabriela Santos <gabrielasnts30@gmail.com>
2025-02-14 13:19:20 -0300

    add devDependencies and change baseUrl

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'src/app/[locale]/blog/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://gabrielacruz.fly.dev/'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
