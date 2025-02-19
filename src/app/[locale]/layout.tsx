ffb6eccbc4b8f4d94bedc3f39be956e42922b3e5
Gabriela Santos <gabrielasnts30@gmail.com>
2025-02-19 01:21:19 -0300

    fix params type in function params

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -13,7 +13,7 @@ export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
 }
 
-export async function generateMetadata({ params }: { params: { locale: string } }) {
+export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
 
   const t = await getTranslations({ locale, namespace: 'LocaleLayout' });
